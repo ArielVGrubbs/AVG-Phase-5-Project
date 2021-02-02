00ec90a660c10d46285555c89ea26be46d32fa7e
ariel_grubbs <arielvictor1234@gmail.com>
2021-02-01 19:46:59 -0600

    removed unused imports in Login VS page

--- react-frontend/src/components/Login.js
+++ react-frontend/src/components/Login.js
@@ -1,14 +1,10 @@
-import React, { useState, useEffect} from 'react';
-import {useSelector, useDispatch } from 'react-redux';
-// import { Link } from "react-router-dom";
-// import { connect } from "react-redux";
+import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
 
 import Avatar from '@material-ui/core/Avatar';
 import Button from '@material-ui/core/Button';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import TextField from '@material-ui/core/TextField';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
-import Checkbox from '@material-ui/core/Checkbox';
 import Link from '@material-ui/core/Link';
 import Grid from '@material-ui/core/Grid';
 import ChildCareIcon from '@material-ui/icons/ChildCare';
@@ -16,7 +12,6 @@ import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
 
-import { login } from '../actions/userActions'
 import { useHistory } from "react-router-dom";
 
 
