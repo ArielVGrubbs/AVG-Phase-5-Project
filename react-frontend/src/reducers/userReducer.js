ee713abb3ad02db7d8ae3b9a6a09682e38d0e979
ariel_grubbs <arielvictor1234@gmail.com>
2021-02-24 14:30:32 -0600

    added thoughts about SET_JOINED in form of comments in userReducer

--- react-frontend/src/reducers/userReducer.js
+++ react-frontend/src/reducers/userReducer.js
@@ -49,6 +49,7 @@ const userReducer = (state = initialState, action) => {
         //         joined: action.joined
         //     }
         // }
+        // Outdated dispatch, user interaction side of this dispatch has already been commented out for a while du to being problematic.
         case 'BAN_USER': {
             let newAllUsers = state.allUsers.filter(u => u.id !== action.channelMember.user_id)
             let bannedUser = state.allUsers.find(u => u.id === action.channelMember.user_id)
