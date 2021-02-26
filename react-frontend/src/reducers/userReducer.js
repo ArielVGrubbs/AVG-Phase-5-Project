c594a709d658bd4ad88fe262752d70a93660b5b6
ariel_grubbs <arielvictor1234@gmail.com>
2021-02-26 16:55:38 -0600

    EOD 2/26

--- react-frontend/src/reducers/userReducer.js
+++ react-frontend/src/reducers/userReducer.js
@@ -9,6 +9,7 @@ const initialState = {
     notifications: [],
     userFetch: false,
     // joined: false
+    // I think this is no longer in use.
 }
 
 const userReducer = (state = initialState, action) => {
