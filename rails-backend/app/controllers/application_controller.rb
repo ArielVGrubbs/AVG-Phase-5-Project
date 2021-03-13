57cbce17283687cab3f80fbb5ddc888bb11a2492
ariel_grubbs <arielvictor1234@gmail.com>
2021-03-13 07:54:41 -0600

    All tests complete, byebugs commented out

--- rails-backend/app/controllers/application_controller.rb
+++ rails-backend/app/controllers/application_controller.rb
@@ -2,6 +2,7 @@ class ApplicationController < ActionController::API
 
     def current_user
         token = request.headers['Auth-Key']
+        # byebug
         begin
             user_id = JWT.decode(token,'SUPER_SECRET_KEY')[0]["user_id"]
             @user = User.find_by(id: user_id)
